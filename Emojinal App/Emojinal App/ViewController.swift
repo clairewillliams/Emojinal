d86220360423c1d5da33e8a83a453820b6c06f1a
Apple <apple@Apples-MacBook-Pro-62.local>
2019-08-09 10:03:12 -0400

    buttons fully work

--- Emojinal App/Emojinal App/ViewController.swift
+++ Emojinal App/Emojinal App/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let emojis = ["🥚" : "Egg", "🐣" : "Hatching Egg", "🐥" : "Chick"]
+    
+    let customMessages = ["Egg" : ["It's okay to feel fried!", "Don't feel scrambled!"], "Hatching Egg" : ["Feeling a little cracked? Hope you have an eggcellent day ahead of you!", "Hatch into today!"], "Chick" : ["You rock, chick!", "Keep on clucking!"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 @IBAction func showMessage(sender: UIButton) {
     //an alert to pop up, show a message and be able to click "ok" to exit
-    let alertController = UIAlertController(title: "Egg", message: "It's okay to feel fried!", preferredStyle: UIAlertController.Style.alert)
+    let selectedEmotion = sender.titleLabel?.text
+    
+    let random = Int.random(in : 0 ..< 2)
+    
+    let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
+    
+    let alertController = UIAlertController(title: selectedEmotion , message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+    
     alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    
     present(alertController, animated: true, completion: nil)
 }
 }
